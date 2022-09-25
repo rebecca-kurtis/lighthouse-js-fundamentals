4873423903e516b66b122205eb23535d1749a730
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-09-25 14:45:08 -0700

    added more function example with console and return

--- hello.js
+++ hello.js
@@ -8,3 +8,16 @@ const sayHello = function(name) {
 }
 
 sayHello("Rebecca");
+
+// using console.log:
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+//using return
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
